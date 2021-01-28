1b845726866f8fb2d897e21dea7bab116ca8bcd6
Abdulrahman Khalid <abdulrahman.elshafei98@gmail.com>
2021-01-28 06:54:33 +0200

    add btm up approach to coin change week5

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -14,24 +14,43 @@ const int INF = 1e9;
 const int N = 1e6 + 5;
 int dp[N];
 VI coins{1, 3, 4};
-int get_change(int n)
+int get_change_btm_up(int amount)
 {
-  if (n == 0)
-    return 0;
-  if (n < 0)
-    return INF;
-  if (dp[n] != 0)
-    return dp[n];
-  int ans = INF;
-  for (size_t i = 0; i < coins.size(); ++i)
-    dp[n] = ans = min(ans, get_change(n - coins[i]) + 1);
-  return ans;
+  // dp[i]: minimum number of coins with denominations 1, 3, 4
+  /* state: i amount */
+  // dp[0] = 0 how many coins to make amount of 0
+  // dp[1] = 1
+  // dp[2] = 2
+  // dp[3] = min(dp[3-1], dp[3-3], d[3-4]) + 1; (+1 as I took one coin 1, 3 or 4)
+  // substitute 3 with i to get transition function
+  // and add condition to make sure i(amount) - coin >=0
+  // transition: dp[i] = min(dp[i-1], dp[i-3], dp[i-4]) + 1
+  dp[0] = 0;
+  for (int i = 1; i <= amount; i++)
+  {
+    dp[i] = INF;
+    for (auto &coin: coins)
+      if (i >= coin)
+        dp[i] = min(dp[i], dp[i-coin]+1);
+  }
+  return dp[amount];
+}
+
+int get_change_top_down(int amount) {
+  if (amount == 0) return 0;
+  // if I calculated dp[amount] ealier I return it
+  if (dp[amount] != 0) return dp[amount]; 
+  dp[amount] = INF;
+  for (auto &coin: coins)
+    if (amount >= coin)
+    dp[amount] = min(dp[amount], get_change_top_down(amount-coin)+1);
+  return dp[amount];
 }
 
 int main()
 {
-  int n;
-  scn(n);
-  print(get_change(n));
+  int amount;
+  scn(amount);
+  print(get_change_btm_up(amount));
   return 0;
 }
